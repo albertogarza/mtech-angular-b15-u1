0988691f4ed7cf82f07ef9de187eccfc5b42a9e5
Alberto Garza <agarza@xactware.com>
2020-08-15 20:21:35 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mtech-angular-d15-u1';
+}
